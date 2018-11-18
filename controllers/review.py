1449b6d3cb8d04dd362814dcc1eb229d3a35ebb0
mrnguuyen <nnbrandon@att.net>
2018-11-18 14:20:16 -0800

    fixed UI

--- controllers/review.py
+++ controllers/review.py
@@ -1,13 +1,1 @@
 
-def get_bike():
-    bike_id = int(request.vars.bike_id)
-    # row = db().select(db.bike.id == bike_id)
-    for row in db(db.bike.id == bike_id).select():
-        result = dict(
-            bike_id = row.id,
-            bike_brand = row.Brand,
-            bike_name = row.Name,
-            bike_desc = row.Description,
-            bike_img_url = row.ImageURL
-        )
-    return response.json(dict(bike=result))
