2b221fad0a02b3c4c03af0ff84695b91715b839a
Curtis <cualee@ucsc.edu>
2018-12-02 17:26:14 -0800

    reviews workinggit add .git add .

--- controllers/api.py
+++ controllers/api.py
@@ -27,3 +27,109 @@ def get_bike():
             bike_img_url = row.ImageURL
         )
     return response.json(dict(bike=result))
+
+@auth.requires_signature()
+def add_post():
+    post_id = db.post.insert(
+        bike_id = int(request.vars.bike_id),
+        post_title=request.vars.post_title,
+        post_content=request.vars.post_content,
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(post_id=post_id))
+
+def get_post_list():
+
+    bike_id = int(request.vars.bike_id)
+    results = []
+    
+
+    if auth.user is None:
+        # Not logged in.
+        #rows = db().select(db.post.ALL, orderby=~db.post.post_time)
+        rows = db(db.post.bike_id == bike_id).select(orderby=~db.post.post_time)
+        for row in rows:
+            results.append(dict(
+                id=row.id,
+                post_title=row.post_title,
+                post_content=row.post_content,
+                post_author=row.post_author,
+                bike_id=row.bike_id,
+                thumb = None,
+                is_author = None
+            ))
+    else:
+        # Logged in.
+        rows = db(db.post.bike_id == bike_id).select(db.post.ALL, db.thumb.ALL,
+                            left=[
+                                db.thumb.on((db.thumb.post_id == db.post.id) & (db.thumb.user_email == auth.user.email)),
+                            ],
+                            orderby=~db.post.post_time)
+        for row in rows:
+            results.append(dict(
+                id=row.post.id,
+                post_title=row.post.post_title,
+                post_content=row.post.post_content,
+                post_author=row.post.post_author,
+                bike_id=row.post.bike_id,
+                thumb = None if row.thumb.id is None else row.thumb.thumb_state,
+                is_author = True if auth.user.email == row.post.post_author else False 
+            ))
+    # For homogeneity, we always return a dictionary.
+    return response.json(dict(post_list=results))
+
+
+@auth.requires_signature()
+def edit_post():
+    post_id = int(request.vars.post_id)
+    post_titlex = request.vars.post_title
+    post_contentx = request.vars.post_content
+    db(db.post.id == post_id).update(post_title = post_titlex)
+    db(db.post.id == post_id).update(post_content = post_contentx)
+    return "ok"
+
+@auth.requires_signature()
+def add_reply():
+    user_email = auth.user.email
+    reply_id = db.reply.insert(
+        post_id = int(request.vars.post_id),
+        reply_content = request.vars.reply_content,
+        reply_author = user_email
+    )
+    result = dict(
+        reply_id = reply_id,
+        reply_author = user_email
+    )
+    return response.json(dict(result))
+
+@auth.requires_signature()
+def edit_reply():
+    reply_id = int(request.vars.reply_id)
+    reply_contentx = request.vars.reply_content
+    db(db.reply.id == reply_id).update(reply_content = reply_contentx)
+    return "ok"
+
+def get_replies():
+    results = []
+    if auth.user is None:
+    # Not logged in.
+        rows = db().select(db.reply.ALL)
+        for row in rows:
+            results.append(dict(
+                id=row.id,
+                reply_author=row.reply_author,
+                post_id=row.post_id,
+                reply_content=row.reply_content,
+                is_author = None
+            ))
+    else:
+        rows = db().select(db.reply.ALL)
+        for row in rows:
+            results.append(dict(
+                id=row.id,
+                reply_author=row.reply_author,
+                post_id=row.post_id,
+                reply_content=row.reply_content,
+                is_author = True if auth.user.email == row.reply_author else False 
+            ))
+    return response.json(dict(reply_list=results))
--- static/js/default_review.js
+++ static/js/default_review.js
@@ -21,9 +21,226 @@ var app = function() {
                 Vue.set(self.vue.bike, '_brand', data.bike.bike_brand);
                 Vue.set(self.vue.bike, '_desc', data.bike.bike_desc);
                 Vue.set(self.vue.bike, '_img', data.bike.bike_img_url);
+                Vue.set(self.vue.bike, '_id', id);
             }
         );
     };
+
+    self.add_post = function (id) {
+
+        console.log(id);
+
+        //var b = self.vue.bike[id];
+
+        // We disable the button, to prevent double submission.
+        $.web2py.disableElement($("#add-post"));
+        var sent_title = self.vue.form_title; // Makes a copy 
+        var sent_content = self.vue.form_content; // 
+        $.post(add_post_url,
+            // Data we are sending.
+            {
+                bike_id: id,
+                post_title: self.vue.form_title,
+                post_content: self.vue.form_content
+
+            },
+            // What do we do when the post succeeds?
+            function (data) {
+                // Re-enable the button.
+                $.web2py.enableElement($("#add-post"));
+                // Clears the form.
+                self.vue.form_title = "";
+                self.vue.form_content = "";
+                // Adds the post to the list of posts. 
+                var new_post = {
+                    bike_id: id,
+                    id: data.post_id,
+                    post_title: sent_title,
+                    post_content: sent_content,
+                    is_author: true
+                };
+                self.vue.post_list.unshift(new_post);
+                // We re-enumerate the array.
+                self.process_posts();
+                self.vue.show_form = false;
+            });
+        // If you put code here, it is run BEFORE the call comes back.
+    };
+
+    self.get_posts = function(id) {
+        console.log("Getting posts for " + id);
+        $.getJSON(get_post_list_url,
+            {
+                bike_id: id,
+            },
+            function(data) {
+                // I am assuming here that the server gives me a nice list
+                // of posts, all ready for display.
+                self.vue.post_list = data.post_list;
+                
+                console.log(self.vue.post_list);
+                // Post-processing.
+                self.process_posts();
+                //return data.post_list;
+            }
+        );
+        
+    };
+
+    self.process_posts = function() {
+        // This function is used to post-process posts, after the list has been modified
+        // or after we have gotten new posts. 
+        // We add the _idx attribute to the posts. 
+        enumerate(self.vue.post_list);
+        // We initialize the smile status to match the like. 
+
+        self.vue.post_list.map(function (e) {
+            // I need to use Vue.set here, because I am adding a new watched attribute
+            // to an object.  See https://vuejs.org/v2/guide/list.html#Object-Change-Detection-Caveats
+            // The code below is commented out, as we don't have smiles any more. 
+            // Replace it with the appropriate code for thumbs. 
+            // // Did I like it? 
+            // // If I do e._smile = e.like, then Vue won't see the changes to e._smile . 
+            // Vue.set(e, '_smile', e.like); 
+
+            Vue.set(e, '_thumb_up', e.thumb == 'u' ? true : false);
+            Vue.set(e, '_thumb_down', e.thumb == 'd' ? true : false);
+            Vue.set(e, '_gray_up', false);
+            Vue.set(e, '_gray_down', false);
+            Vue.set(e, '_editing', false);
+            Vue.set(e, '_show_reply', false);
+            Vue.set(e, '_show_reply_form', false);
+            Vue.set(e, '_reply_list', []);
+        });
+    };
+
+
+    self.edit_post = function(post_idx, post_titlex, post_contentx) {
+        $.web2py.disableElement($("#edit-post"));
+        var p = self.vue.post_list[post_idx];
+        //Make a copy
+        var sent_title = post_titlex;
+        var sent_content = post_contentx;
+        $.post(edit_post_url, {
+            //Send data to api
+            post_title : post_titlex,
+            post_content : post_contentx,
+            post_id : p.id
+        }, function(data) {
+            if(data == "ok") {
+                p.post_title = sent_title;
+                p.post_content= sent_content;
+                is_author= true
+                p._editing = false;
+            }
+        });
+    };
+
+    self.add_reply = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        $.web2py.disableElement($("#add-reply"));
+        var sent_content = self.vue.reply_content;
+        $.post(add_reply_url, {
+            post_id: p.id,
+            reply_content: self.vue.reply_content
+        }, function(data) {
+            self.vue.reply_content = "";
+            var new_reply = {
+                id: data.reply_id,
+                post_id: p.id,
+                reply_content: sent_content,
+                reply_author: data.reply_author,
+                is_author: true
+            };
+            self.vue.reply_list.unshift(new_reply);
+            p._show_reply_form = false;
+            self.process_replies();
+            self.show_reply(post_idx);
+        })
+
+    }
+
+    self.edit_reply = function(reply_idx, reply_contentx) {
+        $.web2py.disableElement($("#edit-reply"));
+        var r = self.vue.reply_list[reply_idx];
+        console.log(reply_contentx);
+        var sent_content = reply_contentx;
+        $.post(edit_reply_url, {
+            reply_id: r.id,
+            reply_content: reply_contentx
+        }, function(data) {
+            if(data == "ok") {
+                r.reply_content = sent_content,
+                is_author = true,
+                r._editing = false;
+            }
+        })
+    };
+
+    self.get_replies = function() {
+        $.getJSON(get_replies_url, function(data) {
+            self.vue.reply_list = data.reply_list;
+            self.process_replies();
+            console.log(self.vue.reply_list);
+        })
+    };
+
+    self.get_reply = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        var this_reply_list = [];
+        self.vue.reply_list.forEach(function(e) {
+            if(e.post_id == p.id) {
+                this_reply_list.push(e);
+            }
+        });
+
+        p._reply_list = this_reply_list;
+        console.log("this is p._reply_list:");
+        console.log(p._reply_list);
+    };
+
+    self.process_replies = function() {
+        enumerate(self.vue.reply_list);
+        self.vue.reply_list.map(function (e) {
+            Vue.set(e, '_editing', false);
+        });
+    };
+
+    self.show_add_form = function() {
+        self.vue.show_form = true;
+    }
+
+    self.show_edit_form = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p._editing = true;
+    }
+
+    self.show_edit_reply_form = function(reply_idx) {
+        var r = self.vue.reply_list[reply_idx];
+        r._editing = true;
+    }
+
+    self.cancel_edit = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p._editing = false;
+    }
+
+    self.show_reply = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p._show_reply = true;
+        self.get_reply(post_idx);
+    }
+
+    self.hide_reply = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p._show_reply = false;
+    }
+
+    self.show_add_reply_form = function(post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p._show_reply_form = true;
+        console.log("show_add_reply_form " + self.vue.show_reply_form );
+    }
 
     // Complete as needed.
     self.vue = new Vue({
@@ -32,14 +249,46 @@ var app = function() {
         props: ['id'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            is_logged_in: is_logged_in,
             bike: {},
+            form_title: "",
+            form_content: "",
+            reply_content: "",
+            post_list: [],
+            thumb_list: [],
+            reply_list: [],
+            show_form: false,
+            show_reply_form: false
         },
         methods: {
             get_bike: self.get_bike,
+            get_posts: self.get_posts,
+            add_post: self.add_post,
+            edit_post: self.edit_post,
+            show_add_form: self.show_add_form,
+            show_edit_form: self.show_edit_form,
+            cancel_edit: self.cancel_edit,
+            add_reply: self.add_reply,
+            
+            get_reply: self.get_reply,
+            get_replies: self.get_replies,
+            show_add_reply_form: self.show_add_reply_form,
+            show_edit_reply_form: self.show_edit_reply_form,
+            show_reply: self.show_reply,
+            hide_reply: self.hide_reply,
+            edit_reply: self.edit_reply,
         }
     });
 
+    // If we are logged in, shows the form to add posts.
+    if (is_logged_in) {
+        $("#add_post").show();
+    }
+
+
     self.get_bike(this.id);
+
+    self.get_posts(this.id);
 
     return self;
 };
